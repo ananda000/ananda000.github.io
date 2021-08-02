9672251e1ea8929fc4ab349e74c0c9faae6d29e9
ananda000 <sindidea6@gmail.com>
2021-08-02 11:04:10 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// event pada saat link di klik 
+$('.page-scroll').on('click', function(e){
+
+	// ambil isi href
+	var tujuan = $(this).attr('href');
+	// tangkap elemen ybs
+	var elemenTujuan = $(tujuan);
+
+	console.top($('body').scrollTop());
+
+	e.preventDefault();
+	
+	
+});
